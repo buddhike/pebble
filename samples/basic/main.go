155ba3fe056ecdd4b1a60071cb3a3253baa16c5e
Buddhike de Silva <buddhiks@amazon.com>
2024-01-27 22:05:18 +1100

    Basic sample with both producer and consumer

--- samples/basic/main.go
+++ samples/basic/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/buddhike/vegas"
+	"github.com/buddhike/vegas/pb"
 )
 
 func main() {
-	p, err := vegas.NewProducer("test", 100, 10)
+	streamName := "test"
+	p, err := vegas.NewProducer(streamName, 100, 10, 1000)
 	if err != nil {
 		panic(err)
 	}
+	go func() {
+		p.Send(context.TODO(), "a", []byte("a"))
+		p.Send(context.TODO(), "a", []byte("a"))
+	}()
+	go func() {
+		c, err := vegas.NewConsumer(streamName, "", func(ur *pb.UserRecord) error {
+			fmt.Println("PK: ", ur.PartitionKey, " Value: ", string(ur.Data))
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+		<-c.Done()
+	}()
 	fmt.Println("Producer started")
 	<-p.Done()
 }
