210e42906fc397bf07a4423ce00a8ad115759c71
Buddhike de Silva <buddhiks@amazon.com>
2024-02-22 10:07:50 +1100

    Readability improvement

--- lang/python/consumer.py
+++ lang/python/consumer.py
@@ -1,9 +1,9 @@
 import vegas 
 
-def callback(ur):
-    print(str(ur.partitionKey) + ":" + ur.data.decode("utf-8"))
+def process_record(r):
+    print(str(r.partitionKey) + ":" + r.data.decode("utf-8"))
 
 stream = "test"
 efo = "arn:aws:kinesis:ap-southeast-2:767660010185:stream/test/consumer/python-consumer:1686199962"
-c = vegas.Consumer(stream, efo, callback)
+c = vegas.Consumer(stream, efo, process_record)
 c.join()
