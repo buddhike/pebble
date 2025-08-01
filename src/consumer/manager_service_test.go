09269a016d0858bf0b62bad03a42fb8ca8ff2a4d
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2025-08-01 15:22:58 +1000

    Manager service tests

--- src/consumer/manager_service_test.go
+++ src/consumer/manager_service_test.go
@@ -36,7 +36,7 @@ func TestAssignToAWorkerWithoutCapacity(t *testing.T) {
 		MaxShards: 0,
 	})
 
-	assert.Equal(t, len(r.Assignments), 0)
+	assert.Empty(t, r.Assignments)
 }
 
 func TestAssignAfterInactivityPeriod(t *testing.T) {
@@ -84,17 +84,20 @@ func TestAssignWhenWorkersAreNotBalanced(t *testing.T) {
 
 	// a gets initial assignment if it calls assign before b
 	r = svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 4})
-	assert.Equal(t, 2, len(r.Assignments))
+	assert.Len(t, r.Assignments, 2)
 
 	// b acquires any reassigned shards
+	// Use a map to keep track of which shard is assigned to b because
+	// shard stealing logic is stable
+	assignments := make(map[string]bool)
 	r = svc.handleAssignRequest(&AssignRequest{WorkerID: "b", MaxShards: 1})
-	assert.Equal(t, 1, len(r.Assignments))
-	assert.Equal(t, "s0", r.Assignments[0].ShardID)
+	assert.Len(t, r.Assignments, 1)
+	assignments[r.Assignments[0].ShardID] = true
 
 	// a should now receive the remaining assignment
 	r = svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 4})
-	assert.Equal(t, 1, len(r.Assignments))
-	assert.Equal(t, "s1", r.Assignments[0].ShardID)
+	assert.Len(t, r.Assignments, 1)
+	assert.False(t, assignments[r.Assignments[0].ShardID])
 }
 
 func TestAssignWhenWorkerBecomesInactiveBeforeItAcquiresReassignments(t *testing.T) {
@@ -154,11 +157,92 @@ func TestAssignWhenWorkerReactivatesBeforePreviousReassignmentOfferIsClearedByAn
 	assert.Len(t, r.Assignments, 1)
 }
 
+func TestAssignForShardSplit(t *testing.T) {
+	svc, w := newTestSubject("s0", "s1,s0", "s2,s0")
+	defer w.CleanUp()
+
+	r := svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 2})
+	assert.Len(t, r.Assignments, 1)
+	assert.Equal(t, "s0", r.Assignments[0].ShardID)
+
+	// a closes the shard
+	aid := r.Assignments[0].ID
+	_, err := svc.handleCheckpointRequest(&CheckpointRequest{AssignmentID: aid, WorkerID: "a", ShardID: "s0", SequenceNumber: "CLOSED"})
+	assert.NoError(t, err)
+
+	// a should get s1 and s2 assigned after s0 is closed
+	r = svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 2})
+	assert.Len(t, r.Assignments, 2)
+	assert.Equal(t, "s1", r.Assignments[0].ShardID)
+	assert.Equal(t, "s2", r.Assignments[1].ShardID)
+}
+
+func TestAssignForShardMerge(t *testing.T) {
+	svc, w := newTestSubject("s0", "s1", "s2,s0,s1")
+	defer w.CleanUp()
+
+	r := svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 2})
+	assert.Len(t, r.Assignments, 2)
+	assert.Equal(t, "s0", r.Assignments[0].ShardID)
+	assert.Equal(t, "s1", r.Assignments[1].ShardID)
+
+	// a closes s0
+	aid := r.Assignments[0].ID
+	_, err := svc.handleCheckpointRequest(&CheckpointRequest{AssignmentID: aid, WorkerID: "a", ShardID: "s0", SequenceNumber: "CLOSED"})
+	assert.NoError(t, err)
+
+	// a should get only s1 because s2 is only available when both parents are closed
+	r = svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 2})
+	assert.Len(t, r.Assignments, 1)
+	assert.Equal(t, "s1", r.Assignments[0].ShardID)
+
+	// a closes s1
+	aid = r.Assignments[0].ID
+	_, err = svc.handleCheckpointRequest(&CheckpointRequest{AssignmentID: aid, WorkerID: "a", ShardID: "s1", SequenceNumber: "CLOSED"})
+	assert.NoError(t, err)
+
+	// a should get s2
+	r = svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 2})
+	assert.Len(t, r.Assignments, 1)
+	assert.Equal(t, "s2", r.Assignments[0].ShardID)
+}
+
+func TestAssignWhenShardIsClosedButChildShardsAreNotYetDiscovered(t *testing.T) {
+	svc, w := newTestSubject("s0")
+	defer w.CleanUp()
+
+	r := svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 2})
+	assert.Len(t, r.Assignments, 1)
+	assert.Equal(t, "s0", r.Assignments[0].ShardID)
+
+	// a closes s0
+	aid := r.Assignments[0].ID
+	_, err := svc.handleCheckpointRequest(&CheckpointRequest{AssignmentID: aid, WorkerID: "a", ShardID: "s0", SequenceNumber: "CLOSED"})
+	assert.NoError(t, err)
+
+	// a should not get any shards because shard discovery has not seen the split yet
+	r = svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 2})
+	assert.Empty(t, r.Assignments, 1)
+
+	w.UpdateShardConfig("s0", "s1,s0", "s2,s0")
+
+	// a should get s1 and s2
+	r = svc.handleAssignRequest(&AssignRequest{WorkerID: "a", MaxShards: 2})
+	assert.Len(t, r.Assignments, 2)
+	assert.Equal(t, "s1", r.Assignments[0].ShardID)
+	assert.Equal(t, "s2", r.Assignments[1].ShardID)
+}
+
 type testWorld struct {
-	CleanUp              func()
-	AdvanceTime          func(by time.Duration)
-	AdvanceTimeBySeconds func(s int)
-	GetEpoc              func() int
+	epoc                int
+	clock               time.Time
+	popCfg              *PopConfig
+	stop                chan struct{}
+	logger              *zap.Logger
+	kds                 *mockKinesis
+	kvs                 *mockKVS
+	shardDiscoverySvc   *ShardDiscoveryService
+	shardDiscoveryTimer chan time.Time
 }
 
 func (w *testWorld) ExpireShardReleaseTimeout() {
@@ -171,9 +255,33 @@ func (w *testWorld) ExpireInactiveWorkers() {
 	w.AdvanceTimeBySeconds(target - epoc)
 }
 
+func (w *testWorld) CleanUp() {
+	close(w.stop)
+	<-w.shardDiscoverySvc.done
+}
+
+func (w *testWorld) AdvanceTimeBySeconds(s int) {
+	w.clock = w.clock.Add(time.Second * time.Duration(s))
+	w.epoc += s
+}
+
+func (w *testWorld) GetEpoc() int {
+	return w.epoc
+}
+
+func (w *testWorld) UpdateShardConfig(shardIDs ...string) {
+	current := len(w.shardDiscoverySvc.GetAll())
+	shards := shardsFromStr(shardIDs...)
+	w.kds.listShards.Return(&kinesis.ListShardsOutput{Shards: shards}, nil)
+	w.shardDiscoveryTimer <- w.clock
+	new := len(w.shardDiscoverySvc.GetAll())
+	for current == new {
+		time.Sleep(time.Millisecond * 50)
+		new = len(w.shardDiscoverySvc.GetAll())
+	}
+}
+
 func newTestSubject(shardIds ...string) (*ManagerService, *testWorld) {
-	epoc := 0
-	clock := time.Time{}
 	popCfg := &PopConfig{
 		WorkerInactivityTimeoutMilliseconds: 5000,
 		ShardReleaseTimeoutMilliseconds:     2000,
@@ -182,36 +290,31 @@ func newTestSubject(shardIds ...string) (*ManagerService, *testWorld) {
 	logger := zap.NewNop()
 	kds := newMockKinesis(shardIds...)
 	kvs := newMockKVS()
+	shardDiscoveryTimerC := make(chan time.Time)
 	sd := NewShardDiscoveryService(popCfg, kds, stop, logger)
+	sd.timer = func(d time.Duration) <-chan time.Time { return shardDiscoveryTimerC }
 	sd.Start()
+
+	w := &testWorld{
+		popCfg:              popCfg,
+		stop:                stop,
+		logger:              logger,
+		kds:                 kds,
+		kvs:                 kvs,
+		shardDiscoverySvc:   sd,
+		shardDiscoveryTimer: shardDiscoveryTimerC,
+	}
 
 	svc := NewManagerService(popCfg, kds, kvs, sd, stop, logger)
 	svc.clock = func() time.Time {
-		return clock
+		return w.clock
 	}
 	svc.SetToInService(1)
 
-	return svc, &testWorld{
-		CleanUp: func() {
-			close(stop)
-			<-sd.done
-		},
-		AdvanceTime: func(by time.Duration) {
-			clock = clock.Add(by)
-		},
-		AdvanceTimeBySeconds: func(s int) {
-			clock = clock.Add(time.Second * time.Duration(s))
-			epoc += s
-		},
-		GetEpoc: func() int {
-			return epoc
-		},
-	}
+	return svc, w
 }
 
-func newMockKinesis(shardIds ...string) *mockKinesis {
-	svc := &mockKinesis{}
-
+func shardsFromStr(shardIds ...string) []types.Shard {
 	var shards []types.Shard
 	for _, shardIdStr := range shardIds {
 		parts := strings.Split(shardIdStr, ",")
@@ -231,7 +334,12 @@ func newMockKinesis(shardIds ...string) *mockKinesis {
 
 		shards = append(shards, shard)
 	}
+	return shards
+}
 
+func newMockKinesis(shardIds ...string) *mockKinesis {
+	svc := &mockKinesis{}
+	shards := shardsFromStr(shardIds...)
 	// If no shard IDs provided, use a default one
 	if len(shards) == 0 {
 		shards = []types.Shard{
@@ -241,7 +349,7 @@ func newMockKinesis(shardIds ...string) *mockKinesis {
 		}
 	}
 
-	svc.On("ListShards", mock.Anything, mock.Anything, mock.Anything).Return(&kinesis.ListShardsOutput{
+	svc.listShards = svc.On("ListShards", mock.Anything, mock.Anything, mock.Anything).Return(&kinesis.ListShardsOutput{
 		Shards: shards,
 	}, nil)
 	return svc
@@ -250,11 +358,15 @@ func newMockKinesis(shardIds ...string) *mockKinesis {
 func newMockKVS() *mockKVS {
 	kvs := &mockKVS{}
 	kvs.On("Get", mock.Anything, mock.Anything, mock.Anything).Return(&clientv3.GetResponse{}, nil)
+	txn := &mockTxn{}
+	txn.On("Commit").Return(&clientv3.TxnResponse{Succeeded: true}, nil)
+	kvs.On("Txn", mock.Anything).Return(txn)
 	return kvs
 }
 
 type mockKinesis struct {
 	mock.Mock
+	listShards *mock.Call
 }
 
 func (m *mockKinesis) AddTagsToStream(ctx context.Context, input *kinesis.AddTagsToStreamInput, optFns ...func(*kinesis.Options)) (*kinesis.AddTagsToStreamOutput, error) {
@@ -424,4 +536,30 @@ func (m *mockKVS) Delete(ctx context.Context, key string, opts ...clientv3.OpOpt
 func (m *mockKVS) Txn(ctx context.Context) clientv3.Txn {
 	args := m.Called(ctx)
 	return args.Get(0).(clientv3.Txn)
+}
+
+type mockTxn struct {
+	mock.Mock
+}
+
+func (t *mockTxn) If(cs ...clientv3.Cmp) clientv3.Txn {
+	return t
+}
+
+// Then takes a list of operations. The Ops list will be executed, if the
+// comparisons passed in If() succeed.
+func (t *mockTxn) Then(ops ...clientv3.Op) clientv3.Txn {
+	return t
+}
+
+// Else takes a list of operations. The Ops list will be executed, if the
+// comparisons passed in If() fail.
+func (t *mockTxn) Else(ops ...clientv3.Op) clientv3.Txn {
+	return t
+}
+
+// Commit tries to commit the transaction.
+func (t *mockTxn) Commit() (*clientv3.TxnResponse, error) {
+	args := t.Called()
+	return args.Get(0).(*clientv3.TxnResponse), args.Error(1)
 }
